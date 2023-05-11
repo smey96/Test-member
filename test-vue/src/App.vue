9c7142baae36af9d3417a00b77d4778546c68980
smey96 <meysoth@gmail.com>
2023-05-11 17:41:07 +0700

    test

--- test-vue/src/App.vue
+++ test-vue/src/App.vue
@@ -7,9 +7,9 @@ import Learnsyntax from './components/Learnsyntax.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
+    <!-- <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
+    </div> -->
     <div class="wrapper">
       <Learnsyntax msg="Test Try" txt="Test Test" />
     </div>
