96c884519baf8c7546db8478684025d20bba46bc
smey96 <meysoth@gmail.com>
2023-05-16 18:43:14 +0700

    import & export

--- test-vue/src/main.js
+++ test-vue/src/main.js
@@ -2,48 +2,42 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import {A, a,test} from '@/utils/test.js'
-// if we try import anything dont have , will report err
+import {A, a, test} from '@/utils/test.js'
+const myA = new A()
+myA.test()
+test();
+// if we try import anything undefine, it will report error
 // import {donthave} from '@/utils/test.js'
 // console.warn(donthave)
 
-
-// if we dont use {}, mean import all ,how use? gerneral we dont import all, use {} get we want is better
 import * as test111 from '@/utils/test.js'
-// import test from '@/utils/test.js' // if test module dont have export default run this will report dont have default;
-// here if i still use test ,will overwrite up test.so we can not use test,should alias other name,
+console.warn("this is test111.a",test111.a) // here will print const a => 1
+// if we dont use {} it mean to import all, how to use? in general we dont import all, use {} to get we want is a better way
+// import test from '@/utils/test.js' 
+// if test module dont have 'export default' when you run this, it will report error (dont have default);
+// here if i still use test, will overwrite up test. so we can not use test, should alias other name,
 // but i dont know, so we can use othername test111
-console.warn(test111.a) // here will print const a =>1
 
-// now try import index.js ,this module have default export
 import index from '@/utils/index'
-// see what is index,u can see index module have all,because index.js already inclue test.js
 console.warn('this is index module',index)
+// now try import index.js, this module have default export
+// see what is index, u can see index module have all, because index.js already inclue test.js
 
-
-// if i want use the things from test.js pass index.js
 // import {A as AA} from '@/utils/index.js'
-
 // const myClassAA = new AA()
-// // here will print 'bb'
-// myClassAA.test();
+// myClassAA.test(); // here will print 'bb'
+// if i want use the things from test.js pass index.js
 
 
 // here is the same import this file @/utils/index.js
+// because above code already have a, so we use 'as', to alias other name 
 import {a as ab} from '@/utils/index'
-//  import {a as ab} from '@/utils'
-// because up have a ,so we use as ,to alias other name 
+// import {a as ab} from '@/utils'
 // import {a as ab} from '@/utils/index.js'
-//  import {a as ab} from '@/utils/'
+// import {a as ab} from '@/utils/'
 
-console.warn('this is a from test.js',a)
-console.warn('this is a from index.js',ab)
-
-
-const myA = new A()
-myA.test()
-
-test();
+console.warn('this is a from test.js', a)
+console.warn('this is a from index.js', ab)
 
 createApp(App).mount('#app')
 
--- test-vue/src/utils/test.js
+++ test-vue/src/utils/test.js
@@ -1,13 +1,13 @@
 
-export const a =1;
+export const a = 1;
 
 export class A{
     test(){
-        console.warn('bb')
+        console.warn('this is class A in test.js, bb')
     }
 }
 
 export function test(){
-    console.warn('aa')
+    console.warn('this is Func test() in test.js, aa')
 }
 
